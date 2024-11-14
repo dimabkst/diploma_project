ffb74baac81a598bcacf1afd762c9215d726716e
dimabkst <dimabkst@gmail.com>
2024-11-14 21:19:06 +0200

    small fixes

--- public/js/territories.js
+++ public/js/territories.js
@@ -1,27 +1,31 @@
 import { customFetch } from './utils.js';
-import { toastError } from './errors.js';
 
 function fetchTerritoriesData() {
   customFetch('/api/territories')
-    .then((response) => response.json())
+    .then((response) => {
+      if (!response) return;
+
+      return response.json();
+    })
     .then((data) => {
+      if (!data) return;
+
       const territoriesList = document.getElementById('territories-list');
       territoriesList.innerHTML = ''; // Clear list
 
-      data?.territories?.forEach((territory) => {
+      data.territories?.forEach((territory) => {
         const li = document.createElement('li');
         li.textContent = territory.name;
         territoriesList.appendChild(li);
       });
-    })
-    .catch((err) => toastError(err));
+    });
 }
 
 export function getTerritoriesPage() {
   return `
-        <h1>Territories</h1>
-        <ul id="territories-list"></ul>
-      `;
+    <h1>Territories</h1>
+    <ul id="territories-list"></ul>
+  `;
 }
 
 export function territoriesPageTriggers() {
