345d92308ceb29272cfcb8d7bee8682efbbd26e4
dimabkst <dimabkst@gmail.com>
2024-11-21 14:27:04 +0200

    try to fix contentsquare connection

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ require('dotenv').config();
 import express from 'express';
 import cookieParser from 'cookie-parser';
 import path from 'path';
+import cors from 'cors';
 import prisma from './db';
 import logger from './utils/logger';
 
@@ -20,6 +21,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, '../public')));
+app.use(cors());
 
 app.use('/api', api);
 app.use(ssr);
