3f54dbe13fb5c60ac04bd29c357a73424b5b5596
dimabkst <dimabkst@gmail.com>
2024-11-21 13:34:08 +0200

    test deployment

--- src/api.ts
+++ src/api.ts
@@ -1,18 +1,18 @@
 import { Router } from 'express';
 import auth from './auth';
-import products from './products';
 import carts from './carts';
-import orders from './orders';
 import manufacturers from './manufacturers';
+import orders from './orders';
+import products from './products';
 import productCategories from './productCategories';
 
 const api: Router = Router();
 
 api.use('/auth', auth);
-api.use('/products', products);
 api.use('/carts', carts);
-api.use('/orders', orders);
 api.use('/manufacturers', manufacturers);
+api.use('/orders', orders);
+api.use('/products', products);
 api.use('/product-categories', productCategories);
 
 export default api;
